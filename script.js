3139a178d9247c8da3e1baa67427bafac21c5fb9
athira_tj <52416790+athiratj@users.noreply.github.com>
2020-09-25 23:57:43 +0530

    completed the first version of navbar and home section

---
+++ script.js
@@ -0,0 +1,24 @@
+//  UI variable
+const navToggler = document.querySelector('.nav-toggler');
+const navMenu = document.querySelector('.site-navbar ul');
+const navLinks = document.querySelectorAll('.site-navbar a');
+
+// load event listners
+allEventListners();
+
+// functions 
+function allEventListners() {
+  navToggler.addEventListener('click', togglerClick);
+  navLinks.forEach( elem => elem.addEventListener('click', navLinkClick));
+}
+
+function togglerClick() {
+  navToggler.classList.toggle('toggler-open');
+  navMenu.classList.toggle('open');
+}
+
+function navLinkClick() {
+  if(navMenu.classList.contains('open')) {
+    navToggler.click();
+  }
+}
